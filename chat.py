0e62e8ee85d9daa08086dd85deea9a19bcc12dbc
Shubham Indoliya <57309059+shubhind@users.noreply.github.com>
2023-05-21 22:19:54 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,41 @@
+import json 
+import numpy as np
+from tensorflow import keras
+
+import random
+import pickle
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+
+def chat():
+    # load trained model
+    model = keras.models.load_model('chat_model')
+
+    # load tokenizer object
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # load label encoder object
+    with open('label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+
+    # parameters
+    max_len = 20
+    
+    while True:
+        print("User: ",end="")
+        inp = input()
+        if inp.lower() == "quit":
+            break
+
+        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                             truncating='post', maxlen=max_len))
+        tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+        for i in data['intents']:
+            if i['tag'] == tag:
+                print("ChatBot :", random.choice(i['responses']))
+
+chat()
